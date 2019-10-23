8dbfca4f41a07d58e8194ca31e584d4b58148f5e
silverlass <silver.lass@khk.ee>
2019-10-23 16:18:04 +0300

    Andmetüübid

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('JS test2');
+// Andmetüübid
+
+//täisarvud
+console.log(10);
+console.log(-4);
+console.log(0);
+
+// Reaalarvud
+console.log(2.5);
+console.log(-7.23);
+console.log(-2.0);
+
+//sõned
+
+console.log('Hello world');
+
+// Tõeväärtused
+
+console.log(true);
+console.log(false);
+
+// Nimekiri - massiiv
+
+console.log([1,2,3,4]);
+
+// Objektid
+
+console.log({a : 1, b : 2});
+console.table({a : 1, b : 2});
+
+
+
