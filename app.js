921e831a687e228ad77d224c30b3a286c465c5d5
silverlass <silver.lass@khk.ee>
2019-10-28 10:13:16 +0200

    Tingimuslaused harjutus 3

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-// tingimuslause küsib vanust
-let vanus = window.prompt('sisesta oma vanus');
-if (vanus<14){
-    window.alert('oled liiga noor');
+//Tingimuslaused harjutus 3
+
+let summaKontol =  prompt('Palju sul raha on?');
+    
+let moneyOut = prompt('Palju sa välja tahad võtta?');
+if (parseInt(moneyOut) > parseInt(summaKontol)){
+    alert('Sul pole nii palju raha');
 }else{
-    window.alert('oled piisavalt vana')
-};
+    let moneyLeft = parseInt(summaKontol) - parseInt(moneyOut);
+    alert('võtsid välja ' + moneyOut + ' Jääk on ' + moneyLeft);
+}
