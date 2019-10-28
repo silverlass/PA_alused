ba8d7e4d8fa04cdda3a6eea6e86a4de1fc2cc579
silverlass <silver.lass@khk.ee>
2019-10-28 11:49:53 +0200

    Tsüklid while algarvud

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 // Tsüklid
 
-// for tsükkel ehk üldtsükkel
+// while tüskkel ehk eelkontrolliga
 
-let numbrid = [];
-//lisame nimekirja sisse juhuslikud arvud
-for (let i =0;i<10;i++){
-    let arv = Math.floor(Math.random() * 10);
-    numbrid.push(arv);
-}
-for (let i = 0; i < numbrid.length;i++){
-    console.log(numbrid[i]);
-}
-for (let kord = 0;kord <numbrid.length;kord++){
-for (let i = 0;i <numbrid.length;i++){
-    let abi;
-    if(numbrid[i]< numbrid[i+1]){
-        abi = numbrid[i];
-        numbrid[i]=numbrid[i+1];
-        numbrid[i+1] = abi;
+let arv = 5;
+let jagaja = 2;
 
+while (arv % jagaja != 0){
+    jagaja++;
+} 
+    if (arv == jagaja){
+        console.log(arv + ' on algarv');
+
+    }else{
+        console.log(arv + ' ei ole algarv');
     }
-}
-    console.log(numbrid);
-}
+
+
+
