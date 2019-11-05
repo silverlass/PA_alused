89f6388f17966c53aa5c97578af7f3ecd64a9520
silverlass <silver.lass@khk.ee>
2019-11-05 15:16:53 +0200

    funktsioon funktsioonis

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // funktsiooniNimi([param1, param2,....paramn]){funktsiooni sisu}
-function paarsuseKontroll(arv){
-
+///function paarsuseKontroll(arv){
+/*
 let jaak = arv % 2;
 if (jaak === 0){
 lause = arv + ' on paaris';
@@ -12,4 +12,19 @@ return lause;
 let paarsus;
 paarsus = paarsuseKontroll(5);
 console.log(paarsus)
+*/
+function ristKylikuPindala(kylg1,kylg2){
+    pindala = kylg1 * kylg2;
+    return pindala;
+}
+//funktsioon, mis väljastab tulemuse ekraanil
+
+function valjasta(kylg1,kylg2){
+    console.log('pikkus = ' + kylg1);
+    console.log('laius = ' + kylg2);
+    let pindala = ristKylikuPindala(kylg1, kylg2);
+    console.log('Pindala = ' + pindala);
+
+}
+valjasta(15,5)
 
