c80bbe9316dd567fdeb0dc9206259ee2ec7e41fc
silverlass <silver.lass@khk.ee>
2019-11-05 14:43:07 +0200

    paarsusekontroll funktsioonina

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-let arv = 4;
+// funktsiooniNimi([param1, param2,....paramn]){funktsiooni sisu}
+function paarsuseKontroll(arv){
+
 let jaak = arv % 2;
-if (jaak ===0){
+if (jaak === 0){
     console.log(arv + ' on paaris');
 }else{
-    console.log(arv + 'on paaritu');
+    console.log(arv + ' on paaritu');
 }
+
+}
+
+paarsuseKontroll(5)
