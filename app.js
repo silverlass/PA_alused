2846085916724e9e5f8a7b58d098a64c6f9c6e71
silverlass <silver.lass@khk.ee>
2019-11-11 10:31:13 +0200

    Scope

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ var a = 1;
 let b = 2;
 const c =3;
 
+if (true){
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log('Plokk: ', a, b, c )
+}
+
+for (var a = 0; a < 10; a++){
+    console.log('Tsükkel', a);
+}
 
 console.log('globaalne',a,b,c);
